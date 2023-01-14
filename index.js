55f264c7e5eaebc244d226694c924597771be7af
Dakota Vikdal <dakota.james19@hotmail.com>
2023-01-14 16:00:52 -0500

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const array = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array, gift) {
+    const emptyArray =[];
+    for (let i = 0; i < array.length; i++) {
+        emptyArray.push(`Thank you, ${array[i]}, for the wonderful ${gift} gift!`)
+    }
+    return emptyArray;
+}
+writeCards(array, "surprise");
+
+
+function countDown() {
+    let number = 10;
+    while(number >= 0) {
+    console.log(number--);
+    }
+    return number;
+}
+countDown();
